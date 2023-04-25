1980db7a7fb7cc6af37c4778f6e05d6c8115e6ea
Anthony Alvarez Delgado <107961797+aalvarez-d@users.noreply.github.com>
2023-04-25 12:50:43 -0600

    More learning

--- Learning1/index.js
+++ Learning1/index.js
@@ -4,3 +4,12 @@
 Asi se comenta multiples lineas
 */
 
+var myName = "Anthony Alvarez Delgado" //Estas pueden cambiar de tipo, es decir, despues le puedo asignar un numero
+
+myName = 8
+
+let myName2 = "Anthony Alvarez Delgado" //Se puede decir que es inmutable (puede cambiar pero por otro str)
+const pi = 3.14 //esto es constante, no cambia por nada
+
+var a; // asi se puede abrir una variable sin asignarle valor (declarar)
+var b = 2; //se declara y se le asigna un valor

8118212b2cd6851a6bfa545740a2fdc4104d4a06
Anthony Alvarez Delgado <107961797+aalvarez-d@users.noreply.github.com>
2023-04-25 13:02:14 -0600

    .

--- Learning1/index.js
+++ Learning1/index.js
@@ -11,5 +11,11 @@ myName = 8
 let myName2 = "Anthony Alvarez Delgado" //Se puede decir que es inmutable (puede cambiar pero por otro str)
 const pi = 3.14 //esto es constante, no cambia por nada
 
-var a; // asi se puede abrir una variable sin asignarle valor (declarar)
+var a; // asi se puede abrir una variable sin asignarle valor (declarar). Abre la variable con valor null
 var b = 2; //se declara y se le asigna un valor
+a = 7; // el punto y coma se puede evitar, pero es mejor ponerlo
+
+console.log(a);
+//En JS el caps tiene importancia. No es lo mismo ae = 2 que AE = 2
+
+
